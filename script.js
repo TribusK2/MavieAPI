21d04de2e8ef316df4dd76e817a3d8c657dd0e1c
TribusK2 <biuro@tribusk2.pl>
2019-10-28 18:51:05 +0100

    Render all movies

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 "use strict"
+
 function submitRequest(){
+    
     let movieTitle = document.getElementById('movieTitle').value;
     if(movieTitle !== ''){
         // prepare data to url search request
@@ -19,46 +21,69 @@ function submitRequest(){
                 // decode response
                 let data = JSON.parse(this.response);
 
-                // create html tags and display searched data
+                // set url request of all searched movies
+                // data.Search.forEach(movie => {
+                //     moviesUrl.push('http://www.omdbapi.com/?i='+movie.imdbID);
+                // });
+
                 let searchResult = document.getElementById('searchResult');
+
                 data.Search.forEach(movie => {
-                    let result = document.createElement('div');
-                    result.setAttribute('class', 'result');
+                    let subXhttp = new XMLHttpRequest();
+                    // let url = 'http://www.omdbapi.com/?apikey=6a2ecd76&i='+movie.imdbID;
+                    // subXhttp.open("GET", url, true);
+                    subXhttp.open("GET", "movie.json", true);
+                    subXhttp.onreadystatechange = function() {
+                        if (this.readyState == 4 && this.status == 200) {
+                            if(this.response){
+                                // let movieDetails = JSON.parse(this.response);
+                               
+                                // create html tags and display searched data
+                                let result = document.createElement('div');
+                                result.setAttribute('class', 'result');
 
-                    let imgResult = document.createElement('img');
-                    imgResult.setAttribute('src', 'resimg.jpg');
+                                let imgResult = document.createElement('img');
+                                imgResult.setAttribute('src', movie.Poster);
 
-                    let resultTitle = document.createElement('h3');
-                    resultTitle.setAttribute('class', 'resultTitle');
+                                let resultTitle = document.createElement('h3');
+                                resultTitle.setAttribute('class', 'resultTitle');
 
-                    let resultYear = document.createElement('h3');
-                    resultYear.setAttribute('class', 'resultYear');
+                                let resultYear = document.createElement('h3');
+                                resultYear.setAttribute('class', 'resultYear');
 
-                    let moreButton = document.createElement('button');
-                    moreButton.setAttribute('class', 'moreButton');
+                                let moreButton = document.createElement('button');
+                                moreButton.setAttribute('class', 'moreButton');
 
-                    searchResult.appendChild(result);
-                    result.appendChild(imgResult);
-                    result.appendChild(resultTitle);
-                    resultTitle.appendChild(document.createTextNode(movie.Title));
-                    result.appendChild(resultYear);
-                    resultYear.appendChild(document.createTextNode(movie.Year));
-                    result.appendChild(moreButton);
-                    moreButton.appendChild(document.createTextNode('Więcej'));
+                                searchResult.appendChild(result);
+                                result.appendChild(imgResult);
+                                result.appendChild(resultTitle);
+                                resultTitle.appendChild(document.createTextNode(movie.Title));
+                                result.appendChild(resultYear);
+                                resultYear.appendChild(document.createTextNode(movie.Year));             
+                            
+                            }
+                            // else{
+                            //     let movieData = {'error': 'brak danych'};
+                            //     document.getElementById("request").innerHTML = data.error;
+                            // }
+                        }
+                    }
+                    subXhttp.send();
+                   
                 });
 
                 // document.getElementById("request").innerHTML = data.Search[0].Title;
-                // document.getElementById("request2").innerHTML = this.response;
+                // document.getElementById("request").innerHTML = movies;
                 }else{
                     let data = {'error': 'brak danych'};
                     document.getElementById("request").innerHTML = data.error;
                 }
             }
         };
-        
-        
+                
         xhttp.send();
     }else{
         document.getElementById('formMessage').innerHTML = "Podaj tytuł filmu";
     }
 }
+
