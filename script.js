cf834c45dc3fc67b1dfef621659a722aadeae226
TribusK2 <biuro@tribusk2.pl>
2019-10-28 11:41:46 +0100

    creating search request

--- script.js
+++ script.js
@@ -2,19 +2,19 @@
 function submitRequest(){
     let movieTitle = document.getElementById('movieTitle').value;
     if(movieTitle !== ''){
-        // prepare data to url request
+        // prepare data to url search request
         let movieTitleUrl = movieTitle.split(' ').join('+');
         
-        // create request
+        // create search request
         let xhttp = new XMLHttpRequest();
-        let url = "http://www.omdbapi.com/?apikey=6a2ecd76&t="+movieTitleUrl+"&plot=full";
-        xhttp.open("GET", url, true);
-        // xhttp.open("GET", "response.json", true);
+        // let url = "http://www.omdbapi.com/?apikey=6a2ecd76&s="+movieTitleUrl+"&type=series";
+        // xhttp.open("GET", url, true);
+        xhttp.open("GET", "response.json", true);
         
         xhttp.onreadystatechange = function() {
             if (this.readyState == 4 && this.status == 200) {            
                 var data = JSON.parse(this.response)
-                document.getElementById("request").innerHTML = data;
+                document.getElementById("request").innerHTML = data.Search[0].Title;
                 document.getElementById("request2").innerHTML = this.response;
                 
             }
